4956921b5729ba88e7ed7a0e7c0cf0bcfb85e5e9
DrKiraDmitry <stalker2807sky@yandex.ru>
2019-08-08 20:55:36 +0700

    2

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,24 +1,32 @@
-var d= document.getElementById('DeusE').childElementCount - 1;
+var d= document.getElementById('DeusE').childElementCount;
 var i=0;
 var tumbler=0;
+var delay= 0;
 
 function deus(){
-
-    if(tumbler==0){
-        document.getElementById('DeusE').children[i].classList.add('DeusExx');
-        i++;
+    if(delay<100){
+        delay++;
     }
-    else{
-        document.getElementById('DeusE').children[i].classList.remove('DeusExx');
-        i--;
-    };
-    console.log(i);
-    if(i==d){
-        tumbler++;
-    };
-    if(i==0){
-        tumbler--;
-    };
+    else
+    if(delay=100){
+        if(tumbler==0){
+            document.getElementById('DeusE').children[i].classList.add('DeusExx');
+            i++;
+        }
+        else{
+            document.getElementById('DeusE').children[i-1].classList.remove('DeusExx');
+            i--;
+        };
+        if(i==d){
+            tumbler++;
+            delay=0;
+        };
+        if(i==0){
+            tumbler--;
+            delay=0;
+        };
+    }
+    
 };
 
 setInterval(deus, 100);
